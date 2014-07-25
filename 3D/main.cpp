7fc5072dc245672e59d81b94bf3f318bec5ef896
RedShank <shashank1693@gmail.com>
2014-07-25 13:16:02 +0530

    3D updated

--- 3D/main.cpp
+++ 3D/main.cpp
@@ -1,3 +1,4 @@
+//MAIN
 #include <iostream>
 #include <math.h>
 #include <unistd.h>
@@ -13,62 +14,113 @@ using namespace std;
 #include "checkFunctions.h"
 
 int main() {
+	
+	//CHECK BETA OR KNUDSEN
+	cout<<"BETA:"<<beta<<endl;
+	//END CHECK
 
-	ofstream file,file2;
-	file.open("Energy.txt");
-	file2.open("Force.txt");
+	ofstream fEnergy, fCenter, fForce, fU, fOmega;
+	fEnergy.open("Energy.txt");
+	fCenter.open("Center_loc.txt");
+	fForce.open("Force.txt");
+	fU.open("Trans_vel.txt");
+	fOmega.open("Rot_vel.txt");
 	int c = 0;
 
+	//REFERENCE VELOCITIES
+	u_ref = u_inlet;
+	w_ref = w_sph_z;
+	//END REF VEL
 
 	lbgrid node(NX,NY,NZ,NX_G,NY_G,NZ_G);
 	lbgrid cell(NX,NY,NZ,NX_G,NY_G,NZ_G);
 	gridtype nodeType(NX,NY,NZ,NX_G,NY_G,NZ_G);
 	gridtype cellType(NX,NY,NZ,NX_G,NY_G,NZ_G);
 
+	gridtype nodeTypeOld(NX,NY,NZ,NX_G,NY_G,NZ_G);
+	gridtype cellTypeOld(NX,NY,NZ,NX_G,NY_G,NZ_G);
+
+	arrayRT rhoNode(NX,NY,NZ,NX_G,NY_G,NZ_G);
+	arrayRT thetaNode(NX,NY,NZ,NX_G,NY_G,NZ_G);
+	arrayRT rhoCell(NX,NY,NZ,NX_G,NY_G,NZ_G);
+	arrayRT thetaCell(NX,NY,NZ,NX_G,NY_G,NZ_G);
+
+	//Initialize
 	initializeWithZeroVel(node,cell);
-	//setTypeSphere(nodeType,cellType);
-	setTypeWall(nodeType, cellType);
-	//cout<<beta<<endl;
+	setType(nodeType,cellType,nodeTypeOld,cellTypeOld);
+	//End initialize
+		
+	//First print-check
+	printStokesCorrection();
+	printU(fU);
+	printOmega(fOmega);
+	//End check
+
+	//START SIMULATION
 	for (int t = 0; t < SIMULATION_TIME; t++) {
-//		cout<<"After periodic-----";
-//		printGlobalRho(node,cell);
+		//refill(nodeTypeOld, cellTypeOld, nodeType, cellType, node, cell);
+		checkInit(node,nodeType);
+
 		collide(node);
 		collide(cell);
+		#ifdef INLET
+			setInletOutlet(node,cell);
+			periodic23(node);
+			periodic23(cell);	
+		#else
+			periodic123(node);
+			periodic123(cell);
+		#endif
 
-		setInletOutlet(node,cell);
-		periodic23(node);
-		periodic23(cell);
-		
 		stream(node,cell);
-		setInletOutletCorrection(node,cell);
-		setInternalBoundary(node,cell,nodeType,cellType);
-		if (t%100 == 0) {
-			printGlobalRho(node,cell,nodeType,cellType,file);
-			file2<<forceX/(u_inlet*u_inlet*radius*radius*3.14*0.5)<<endl;		
-		}
-		//printEachTime(node,t,c);
-		//printVelX(node,nodeType,t,c);
-		if(t%100==0) printAllVel(node,nodeType);// printEnergy(node,file);
-		//cout<<t<<endl;
-	}
-	//printAllVel(node,nodeType);
-	file.close();
-	file2.close();
 
-}
+//		forceXOld = forceX;
+//		torqueZOld = torqueZ;
+//		forceYOld = forceY;
+//		torqueYOld = torqueY;
+//		forceZOld = forceZ;
+//		torqueXOld = torqueX;
+
+		setInternalBoundary(node, cell, nodeType, cellType, rhoNode, thetaNode, rhoCell, thetaCell);
+//		setBoundaryCorrection(node,cell,nodeType,cellType);
+		#ifdef INLET 
+			setInletOutletCorrection(node,cell);
+		#endif
+
+		#ifdef MOVING_SPHERE
+			updateCMObj(nodeTypeOld, nodeType, cellTypeOld, cellType);
+			setTypeSphere(nodeType, cellType);
+		#endif
 		
-//*******To check if feq is working**********	
+		//CHECK CENTER
+		if (center_x > NX - 2*radius || center_y > NY - 2*radius || center_z >NZ - 2*radius || center_x < 2*radius || center_y < 2*radius || center_z < 2*radius) break;
+		//END CENTER CHECK
 
-//	//CHECK FEQ
-//	myReal f[27];	
-//	myReal rho,ux,uy,uz,t,r;
-//	rho=0.0871;ux=0.032543;uy=0.02112;uz=0.01326;t=1.0/12.0;r=0.0;
-//	cout<<rho<<" "<<ux<<" "<<uy<<" "<<uz<<" "<<t<<" "<<endl;
-//	getFeq(f,rho,ux,uy,uz,t);
-//	//cout<<f[3]<<endl;
-//	getMoments(f,rho,ux,uy,uz,t,r);
-//	cout<<rho<<" "<<ux<<" "<<uy<<" "<<uz<<" "<<t<<" "<<endl;
-//	//END CHECK FEQ	
 
+		//******PRINT ROUTINES*********
+		if (t%10 == 0) {
+//			printCenterLocation(fCenter);
+			printForceAndTorque(fForce);			
+			printGlobalRho(node,cell,nodeType,cellType,fEnergy);
+//			printAllVel(node,nodeType);
+//			printU(fU);
+//			printOmega(fOmega);			
+//			printVelX(node,nodeType,t,c);
+		}
+	
+		//*******END PRINT ***********
+
+
+	}
+	//END SIMULATION
+	//close files
+	fCenter.close();
+	fEnergy.close();
+	fForce.close();
+	fU.close();
+	fOmega.close();
+}
+
+//END MAIN
 		
 	
